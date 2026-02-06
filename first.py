5821e10a6c56e11dce2e41c2ee9affe75cbacda4
Ayan270805 <singhayan3855@gmail.com>
2026-02-06 15:50:34 +0530

    my python file

---
+++ first.py
@@ -0,0 +1,1 @@
+print("My Name Is Ayan Singh\nI am a student of UCER")
---
+++ second.py
@@ -0,0 +1,10 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+if a>b and a>c:
+    print("a is large")
+elif b>a and b>c:
+    print("b is large")
+else:
+    print("c is large")
+
